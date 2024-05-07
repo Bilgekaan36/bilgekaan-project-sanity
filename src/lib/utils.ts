d1a88acea3fe72b8676262eeb8ef355805f2a7c5
Bilgekaan36 <b.yilmaz@workdigital.de>
2024-05-07 19:47:09 +0200

    bugfix failed build because of utils wrong import dependency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,19 @@
-import { client } from '@/sanity/sanity.client'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import imageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from '@sanity/image-url/lib/types/types'
-import { QueryParams } from 'next-sanity'
+import { createClient, type QueryParams } from 'next-sanity'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function getStrapiURL() {
-  return process.env.NEXT_PUBLIC_STRAPI_URL ?? 'http://127.0.0.1:1337'
-}
+export const client = createClient({
+  projectId: '8fnf6911',
+  dataset: 'production',
+  apiVersion: '2024-01-01',
+  useCdn: false,
+})
 
 const { projectId, dataset } = client.config()
 
@@ -19,46 +21,6 @@ export const urlFor = (source: SanityImageSource) =>
   projectId && dataset
     ? imageUrlBuilder({ projectId, dataset }).image(source)
     : null
-
-export function flattenAttributes(data: any): any {
-  // Check if data is a plain object; return as is if not
-  if (
-    typeof data !== 'object' ||
-    data === null ||
-    data instanceof Date ||
-    typeof data === 'function'
-  ) {
-    return data
-  }
-
-  // If data is an array, apply flattenAttributes to each element and return as array
-  if (Array.isArray(data)) {
-    return data.map((item) => flattenAttributes(item))
-  }
-
-  // Initialize an object with an index signature for the flattened structure
-  let flattened: { [key: string]: any } = {}
-
-  // Iterate over each key in the object
-  for (let key in data) {
-    // Skip inherited properties from the prototype chain
-    if (!data.hasOwnProperty(key)) continue
-
-    // If the key is 'attributes' or 'data', and its value is an object, merge their contents
-    if (
-      (key === 'attributes' || key === 'data') &&
-      typeof data[key] === 'object' &&
-      !Array.isArray(data[key])
-    ) {
-      Object.assign(flattened, flattenAttributes(data[key]))
-    } else {
-      // For other keys, copy the value, applying flattenAttributes if it's an object
-      flattened[key] = flattenAttributes(data[key])
-    }
-  }
-
-  return flattened
-}
 
 export async function sanityFetch<QueryResponse>({
   query,
