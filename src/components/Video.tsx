d4820d19a859d200350ee5987ab8fdcc991d9d29
Bilgekaan36 <b.yilmaz@workdigital.de>
2024-05-08 13:15:35 +0200

    implement new showcases version with new ui

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,13 +1,6 @@
 export function Video({ url }: { url: string }) {
   return (
-    <video
-      width="auto"
-      className="!h-full !object-cover"
-      preload="none"
-      autoPlay
-      loop
-      muted
-    >
+    <video width="auto" className="!h-full !object-cover" autoPlay loop muted>
       <source src={url} type="video/mp4" />
       {/* <track src={captions} kind="subtitles" srcLang="en" label="English" /> */}
       Your browser does not support the video tag.
