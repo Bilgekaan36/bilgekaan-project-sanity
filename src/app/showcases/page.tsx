d1849d6dccce937b4f9184973234e3b5ae4c656b
Bilgekaan36 <b.yilmaz@workdigital.de>
2024-05-08 14:00:41 +0200

    implement new showcases version with new ui

--- src/app/showcases/page.tsx
+++ src/app/showcases/page.tsx
@@ -1,26 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 import { ContainerScroll } from '@/components/ui/container-scroll-animation'
 import { Video } from '@/components/Video'
 import { sanityFetch } from '@/lib/utils'
 import { SanityDocument } from 'next-sanity'
-import { Container } from '@/components/Container'
 
-const SHOWCASES_QUERY = `*[_type == "showcase"]{
-  "showcases": showcases[]{
-    title,
-    subtitle,
-    video->{
-      videos{
-      asset->{
-          url,
-          _id
-        }
-      }
-    },
-    path
-  }
+const SHOWCASES_QUERY = `*[_type == "showcase" && title == "Portfolio Image Showcases"]{
+  showcases
 }`
 
 export default async function Showcases() {
@@ -33,24 +21,38 @@ export default async function Showcases() {
   const { showcases } = showcasesData
 
   return (
-    <Container className="mt-16 sm:mt-32">
-      <>
-        <h1 className="text-2xl font-semibold text-black sm:text-4xl dark:text-white">
-          Some of my selected
-          <br />
-          <br />
-          <span className="mt-1 text-4xl font-bold leading-none md:text-[6rem]">
-            Showcases
-          </span>
-        </h1>
-      </>
+    <div className="flex flex-col overflow-hidden">
       {showcases.map((showcase: any, index: any) => (
-        <div key={index} className="my-20">
+        <ContainerScroll
+          key={index}
+          titleComponent={
+            showcase.title && (
+              <>
+                <h1 className="text-4xl font-semibold text-black dark:text-white">
+                  {showcase?.title}
+                  <br />
+                  <span className="mt-1 text-4xl font-bold leading-none md:text-[6rem]">
+                    {showcase?.subtitle}
+                  </span>
+                </h1>
+              </>
+            )
+          }
+        >
           <Link href={showcase.path ? showcase.path : '#'}>
-            <Video url={showcase ? showcase.video.videos.asset.url : ''} />
+            <Image
+              src={showcase.url}
+              alt={showcase.url}
+              height={720}
+              width={1400}
+              className="mx-auto h-full rounded-2xl object-cover object-left-top"
+              draggable={false}
+            />
+            {/* @ts-ignore */}
+            {/* <Video url={showcase ? showcase.video.videos.asset.url : ''} /> */}
           </Link>
-        </div>
+        </ContainerScroll>
       ))}
-    </Container>
+    </div>
   )
 }
