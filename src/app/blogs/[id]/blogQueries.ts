ef06cd815ac2533df35ad4a84682b1ff1cd05ad3
Bilgekaan36 <b.yilmaz@workdigital.de>
2025-05-26 13:31:42 +0200

    remove log

--- src/app/blogs/[id]/blogQueries.ts
+++ src/app/blogs/[id]/blogQueries.ts
@@ -3,7 +3,6 @@ import type { SanityDocument } from '@sanity/client'
 
 export async function fetchBlogById(params: any) {
   const BLOG_QUERY = `*[_type == "blog" && _id == $id][0]`
-  console.log('params', params)
 
   return await sanityFetch<SanityDocument[]>({ query: BLOG_QUERY, params })
 }
